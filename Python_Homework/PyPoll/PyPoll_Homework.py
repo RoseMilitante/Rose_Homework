82030041d5f606c2cfd443ce78d1aefc2fc183fb
All4roses <53385168+RoseMilitante@users.noreply.github.com>
2019-09-12 21:47:03 -0700

    Updated to calculate Pypoll winner better. Still unsuccessful in having Pypoll output file contain candidate stats

--- Python_Homework/PyPoll/PyPoll_Homework.py
+++ Python_Homework/PyPoll/PyPoll_Homework.py
@@ -2,63 +2,79 @@
 import os
 import csv
 
-#filepath
+# filepath
 csvpath = os.path.join("Resources", "election_data.csv")
 
-#declare my variables
-names = {}
-votes_received = 0
-winner = []
-votes = 0
+# declare my variables
+names = []
+candidates = []
+vote_counter = []
+percentage_of_votes = []
 
 # Find the total number of votes cast
 # Since each row in the csv file is a vote
-#I will just count the lines in the file
+# I will just count the lines in the file
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
     
     total_votes = len(list(csvreader))
 
-#print total votes to the terminal
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+ 
+    #take in all the names people voted for and store that into a list
+    for x in csvreader:
+        names.append(x[2])
+
+#sort the list of names alphabetically        
+sorted_names = sorted(names)
+
+#create a new list called candidates of only the unique names
+for someone in range(total_votes):
+    if sorted_names[someone-1] != sorted_names[someone]:
+        candidates.append(sorted_names[someone])
+
+#come up with a total of unique candidte names
+total_candidates = len(candidates)        
+
+
+#using a double for loop, create 2 lists
+#one to store the total votes each candidate received
+#another to store the percentage of votes each candidate received
+for people in range(total_candidates):
+    votes = 0
+    
+    for avote in range(total_votes):
+        if sorted_names[avote] == candidates[people]:
+            votes += 1 
+    vote_counter.append(votes)
+    percentage_of_votes.append(round((votes/total_votes * 100),2))    
+
+# print the totals to the terminal
 print("-"*25)
 print("Election Results")
 print("-"*25)
 print(f"Total Votes: {total_votes}")
 print("-"*25)
 
-# A complete list of candidates who received votes
-# here we open up the csv file and read it, skipping the header
-with open(csvpath, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader)
+combined_stats = zip(candidates, vote_counter, percentage_of_votes)
+for each in combined_stats:
+    print(f'{each[0]}: {each[2]}% ({each[1]})')
 
-    #look through every row in the csv file and
-    #Use candidate_name with each candidate's name 
-    #use these names as the key. 
-    #look through each row and if that name comes up, add 1 to their each names's counts
-    for row in csvreader:
-        candidate_name = row[2]
-        
-        if candidate_name in names.keys():
-             names[candidate_name] += 1
-        else:
-             names[candidate_name] = 1
-    
-    for candidate_name in names:
-        #The percentage of votes each candidate won
-        percentage = round((names[candidate_name])/total_votes * 100)
-        #The total number of votes each candidate won
-        votes_received = names[candidate_name]
-        print(f'{candidate_name}: {percentage}% ({votes_received})')
-
-# The winner of the election based on popular vo
-winner = list(names.keys())
+# figure out who won by comparing which number in my
+# vote_counter list is the largest
+# and then you take that place holder called 'most'
+# and find that same spot in the candidates list
+# to find the winner's name
+for most in range(total_candidates):
+    if vote_counter[most] > vote_counter[most - 1]:
+        winner = candidates[most]
 
 print("-"*25)
-print(f'Winner: {winner[0]}')
+print(f'Winner: {winner}')
 print("-"*25)
-
 
 # Export analysis to a text file with the results
 # Specifiy the file to write to
@@ -68,7 +84,7 @@ output_path = os.path.join("output","pypoll_output.txt")
 with open(output_path, 'w') as file:
     file.write("Election Results\n")
     file.write("---------------------------\n")
-    file.write(f"Total Votes: {total_votes}\n")
+    file.write(f"Total Votes: {total_votes}\n")      
     file.write("---------------------------\n")
-    file.write(f"Winner: {winner[0]}\n")
+    file.write(f"Winner: {winner}\n")
     file.write("---------------------------\n")
