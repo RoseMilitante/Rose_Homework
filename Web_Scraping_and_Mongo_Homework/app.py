b5be8c90a2fe55031f1bab048d2fcb0efa0ae5bc
All4roses <53385168+RoseMilitante@users.noreply.github.com>
2019-11-25 18:41:00 -0800

    updated some homework files

--- Web_Scraping_and_Mongo_Homework/app.py
+++ Web_Scraping_and_Mongo_Homework/app.py
@@ -1,41 +1,37 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect
 # Import our pymongo library, which lets us connect our Flask app to our Mongo database.
-import pymongo
-
+from flask_pymongo import PyMongo
 # Import scrape_mars
 import scrape_mars
 
-# Create an instance of our Flask app.
+# Create an instance of Flask app.
 app = Flask(__name__)
 
-# Create connection variable
-conn = 'mongodb://localhost:27017/mars_db'
-
-# Pass connection to the pymongo instance.
-client = pymongo.MongoClient(conn)
-
-# Connect to a database. Will create one if not already available.
-db = client.mars_db
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
 # Drops collection if available to remove duplicates
-db.mars.drop()
+#db.mars.drop()
 
 # Set route
 @app.route('/')
-def index():
+def home():
     # Store the entire mars collection in a list
-    mars = list(db.mars.find())
+    mars = mongo.db.collection.find_one()
     
     # Return the template with the mars list passed in
     return render_template('index.html', mars=mars)
 
+# Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-    mars = client.db.mars
+
+    # mars = client.db.mars
     mars_data = scrape_mars.scrape()
-    mars.update({}, mars_data)
-    return "Done scraping."
+    mongo.db.collection.update({}, mars_data, upsert=True)
+    
+    # Redirect back to home page
+    return redirect("/")
 
 
 if __name__ == "__main__":
--- Web_Scraping_and_Mongo_Homework/scrape_mars.py
+++ Web_Scraping_and_Mongo_Homework/scrape_mars.py
@@ -1,8 +1,8 @@
 # Dependencies
 from bs4 import BeautifulSoup
-import requests
 from splinter import Browser
 import pandas as pd
+import time
 
 def init_browser():
     executable_path = {'executable_path': 'chromedriver.exe'}
@@ -10,10 +10,9 @@ def init_browser():
 
 def scrape():
     mars_complete_data = {}
-    browswer = init_browser()
+    browser = init_browser()
 
     # NASA Mars News
-    # URL of page to be scraped
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
     time.sleep(2)
@@ -21,16 +20,12 @@ def scrape():
     html = browser.html
     mars_soup = BeautifulSoup(html, 'html.parser')
 
-    #mars_complete_data['title'] = mars_soup.find('div', class_="content_title").get_text()
-    #mars_complete_data['paragraph'] = mars_soup.find('div', class_="article_teaser_body").get_text()
-
     articles = mars_soup.find_all('div', class_="list_text")
 
     for article in articles:
         news_titles = article.find('div', class_="content_title")
         mars_complete_data['title'] = news_titles.find('a').get_text()
         mars_complete_data['paragraph'] = article.find('div', class_="article_teaser_body").get_text()
-
 
     # JPL Mars Space Images
     jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
@@ -43,7 +38,7 @@ def scrape():
         photo_references = image['data-fancybox-href']
         images.append(photo_references)
         
-        mars_complete_data[featured_image_url] = 'https://www.jpl.nasa.gov'+ photo_references
+        mars_complete_data['featured_image_url'] ="ttps://www.jpl.nasa.gov" + photo_references
     
     # Mars Weather
     weather_url = 'https://twitter.com/marswxreport?lang=en'
@@ -65,98 +60,44 @@ def scrape():
     # Mars Facts
     mars_facts_url = 'https://space-facts.com/mars/'
     mars_table = pd.read_html(mars_facts_url)
- # ## Mars Hemispheres
-# URLs for Mars hemisphers from of Astrogeology site
-cerberus_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced'
-schiaparelli_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced'
-syrtis_major_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced'
-valles_marineris_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced'
-
-# ### Cerberus Hemisphere
-browser.visit(cerberus_url)
-html = browser.html
-cerberus_soup = BeautifulSoup(html, 'html.parser')
-cerberus_image = cerberus_soup.find_all('div', class_='wide-image-wrapper')
-for image in cerberus_image:
-    photo = image.find('li')
-    full_res_photo = photo.find('a')['href']
-    print(full_res_photo)
-cerberus_title = cerberus_soup.find('h2', class_='title').text
-cerberus_hemisphere = {"title": cerberus_title, "img-url": full_res_photo}
-
-# ### Schiaparelli Hemisphere
-browser.visit(schiaparelli_url)
-html = browser.html
-schiaparelli_soup = BeautifulSoup(html, 'html.parser')
-schiaparelli_image = schiaparelli_soup.find_all('div', class_='wide-image-wrapper')
-for image in schiaparelli_image:
-    sch_photo = image.find('li')
-    full_res_photo = sch_photo.find('a')['href']
-schiaparelli_title = schiaparelli_soup.find('h2', class_='title').text
-schiaparelli_hemisphere = {"title": schiaparelli_title, "img_url": full_res_photo}
-
-# ### Syrtis Major Hemisphere
-browser.visit(syrtis_major_url)
-html = browser.html
-syrtis_major_soup = BeautifulSoup(html, 'html.parser')
-syrtis_major_image = syrtis_major_soup.find_all('div', class_='wide-image-wrapper')
-for image in syrtis_major_image:
-    sm_photo = image.find('li')
-    full_res_photo = sm_photo.find('a')['href']
-syrtis_major_title = syrtis_major_soup.find('h2', class_='title').text
-syrtis_major_hemisphere = {"title": syrtis_major_title, "img_url": full_res_photo}
-
-# ### Valles Marineris Hemisphere
-browser.visit(valles_marineris_url)
-html = browser.html
-valles_marineris_soup = BeautifulSoup(html, 'html.parser')
-valles_marineris_image = valles_marineris_soup.find_all('div', class_='wide-image-wrapper')
-for image in valles_marineris_image:
-    vm_photo = image.find('li')
-    full_res_photo = vm_photo.find('a')['href']
-valles_marineris_title = valles_marineris_soup.find('h2', class_='title').text
-valles_marineris_hemisphere = {"title": valles_marineris_title, "img_url": full_res_photo}
-#appending all 4 dictionaries into 1 list
-mars_hemispheres = [cerberus_hemisphere, schiaparelli_hemisphere, syrtis_major_hemisphere, valles_marineris_hemisphere]
-
-mars_complete_data['mars_hemispheres'] = mars_hemispheres   table = mars_table[0]
+    # only need the data in table 1
+    table = mars_table[0]
     table.columns = ['Description', 'Facts']
-    table.set_index(['Description'])
-    table_data_html = table.to_html()
+    temp_table = table.set_index(['Description'])
+    table_data_html = temp_table.to_html()
     table_data_html = table_data_html.replace("\n","")
-    mars_complete_data['table'] = table_data_html
+    mars_complete_data['mars_facts'] = table_data_html
 
-    # ## Mars Hemispheres
+    # Mars Hemispheres
     # URLs for Mars hemisphers from of Astrogeology site
     cerberus_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced'
     schiaparelli_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced'
     syrtis_major_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced'
     valles_marineris_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced'
 
-    # ### Cerberus Hemisphere
+    # Cerberus Hemisphere
     browser.visit(cerberus_url)
     html = browser.html
     cerberus_soup = BeautifulSoup(html, 'html.parser')
     cerberus_image = cerberus_soup.find_all('div', class_='wide-image-wrapper')
     for image in cerberus_image:
-        photo = image.find('li')
-        full_res_photo = photo.find('a')['href']
-        print(full_res_photo)
-    cerberus_title = cerberus_soup.find('h2', class_='title').text
-    cerberus_hemisphere = {"title": cerberus_title, "img-url": full_res_photo}
+        cerb_photo = image.find('li')
+        full_res_photo = cerb_photo.find('a')['href']
+    cerberus_title = cerberus_soup.find('h2', class_='title').text.replace('Enhanced', '')
+    cerberus_hemisphere = {"title": cerberus_title, "img_url": full_res_photo}
 
-    # ### Schiaparelli Hemisphere
+    # Schiaparelli Hemisphere
     browser.visit(schiaparelli_url)
     html = browser.html
     schiaparelli_soup = BeautifulSoup(html, 'html.parser')
     schiaparelli_image = schiaparelli_soup.find_all('div', class_='wide-image-wrapper')
     for image in schiaparelli_image:
-        sch_photo = image.find('li') 
+        sch_photo = image.find('li')
         full_res_photo = sch_photo.find('a')['href']
-    schiaparelli_title = schiaparelli_soup.find('h2', class_='title').text
+    schiaparelli_title = schiaparelli_soup.find('h2', class_='title').text.replace('Enhanced', '')
     schiaparelli_hemisphere = {"title": schiaparelli_title, "img_url": full_res_photo}
 
-    # ### Syrtis Major Hemisphere
+    # Syrtis Major Hemisphere
     browser.visit(syrtis_major_url)
     html = browser.html
     syrtis_major_soup = BeautifulSoup(html, 'html.parser')
@@ -164,10 +105,10 @@ mars_complete_data['mars_hemispheres'] = mars_hemispheres   table = mars_table[0
     for image in syrtis_major_image:
         sm_photo = image.find('li')
         full_res_photo = sm_photo.find('a')['href']
-    syrtis_major_title = syrtis_major_soup.find('h2', class_='title').text
+    syrtis_major_title = syrtis_major_soup.find('h2', class_='title').text.replace('Enhanced', '')
     syrtis_major_hemisphere = {"title": syrtis_major_title, "img_url": full_res_photo}
 
-    # ### Valles Marineris Hemisphere
+    # Valles Marineris Hemisphere
     browser.visit(valles_marineris_url)
     html = browser.html
     valles_marineris_soup = BeautifulSoup(html, 'html.parser')
@@ -175,13 +116,17 @@ mars_complete_data['mars_hemispheres'] = mars_hemispheres   table = mars_table[0
     for image in valles_marineris_image:
         vm_photo = image.find('li')
         full_res_photo = vm_photo.find('a')['href']
-    valles_marineris_title = valles_marineris_soup.find('h2', class_='title').text
+    valles_marineris_title = valles_marineris_soup.find('h2', class_='title').text.replace('Enhanced', '')
     valles_marineris_hemisphere = {"title": valles_marineris_title, "img_url": full_res_photo}
+    
     #appending all 4 dictionaries into 1 list
     mars_hemispheres = [cerberus_hemisphere, schiaparelli_hemisphere, syrtis_major_hemisphere, valles_marineris_hemisphere]
 
     mars_complete_data['mars_hemispheres'] = mars_hemispheres
+ 
+    # Close the browser after scraping
+    browser.quit()
 
-
+    # Return results
     return mars_complete_data
 
