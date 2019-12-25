e0dcc27998bdc6f960ef3ffb1a009451678e8204
All4roses <53385168+RoseMilitante@users.noreply.github.com>
2019-12-25 01:38:18 -0800

    updated

--- D3_Homework/assets/js/app.js
+++ D3_Homework/assets/js/app.js
@@ -1,22 +1,22 @@
 // @TODO: YOUR CODE HERE!
 
 // Store width and height parameters to be used in later in the canvas
-var svgWidth = 900;
+var svgWidth = 950;
 var svgHeight = 600;
 
 // Set svg margins 
 var margin = {
   top: 30,
   right: 30,
-  bottom: 75,
-  left: 80
+  bottom: 80,
+  left: 90
 };
 
 // Create the width and height based svg margins and parameters to fit chart group within the canvas
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create the canvas to append the SVG group that contains the states data
+// Create the canvas to append the SVG group that contains the given data
 // Give the canvas width and height calling the variables predifined.
 var svg = d3.select("#scatter")
   .append("svg")
@@ -29,81 +29,73 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-//var file = "https://raw.githubusercontent.com/the-Coding-Boot-Camp-at-UT/UTAUS201807DATA2/master/homework-instructions/16-D3/Instructions/data/data.csv?token=AlrQ1AsqqfeBIqdGwDr3Q8EXWIADFBmmks5b40y2wA%3D%3D"
 var file = "assets/data/data.csv"
 
 // Function is called and passes csv data
 d3.csv(file).then(successHandle, errorHandle);
 
-// Use error handling function to append data and SVG objects
-// If error exist it will be only visible in console
 function errorHandle(error) {
   throw err;
 }
 
-// Function takes in argument statesData
-function successHandle(statesData) {
+function successHandle(givenData) {
 
   // Loop through the data and pass argument data
-  statesData.map(function (data) {
-    data.poverty = +data.poverty;
-    data.obesity = +data.obesity;
-  });
+    givenData.map(function(data) {
+      data.smokes = +data.smokes;
+      data.age = +data.age;
+    });
 
   //  Create scale functions
   // Linear Scale takes the min to be displayed in axis, and the max of the data
   var xLinearScale = d3.scaleLinear()
-    .domain([8.1, d3.max(statesData, d => d.poverty)])
+    .domain([8.1, d3.max(givenData, d => d.age)])
     .range([0, width]);
 
   var yLinearScale = d3.scaleLinear()
-    .domain([20, d3.max(statesData, d => d.obesity)])
+    .domain([20, d3.max(givenData, d => d.smokes)])
     .range([height, 0]);
 
   // Create axis functions by calling the scale functions
 
-  var bottomAxis = d3.axisBottom(xLinearScale)
-    // Adjust the number of ticks for the bottom axis  
-    .ticks(7);
+  var bottomAxis = d3.axisBottom(xLinearScale);
   var leftAxis = d3.axisLeft(yLinearScale);
-
-
-
 
   // Append the axes to the chart group 
   // Bottom axis moves using height 
   chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
+  
   // Left axis is already at 0,0
   // Only append the left axis 
   chartGroup.append("g")
     .call(leftAxis);
 
 
-  // Create Circles for scatter plot
+  // Create Circles
   var circlesGroup = chartGroup.selectAll("circle")
-    .data(statesData)
+    .data(givenData)
     .enter()
     .append("circle")
-    .attr("cx", d => xLinearScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.obesity))
-    .attr("r", "13")
-    .attr("fill", "#788dc2")
-    .attr("opacity", ".75")
+    .attr("cx", d => xLinearScale(d.age))
+    .attr("cy", d => yLinearScale(d.smokes))
+    .attr("r", "15")
+    .attr("fill", "lightblue")
+    .attr("opacity", ".65");
 
 
   // Append text to circles 
 
-  var circlesGroup = chartGroup.selectAll()
-    .data(statesData)
+  var circleLabel = chartGroup.selectAll()
+    .data(givenData)
     .enter()
     .append("text")
-    .attr("x", d => xLinearScale(d.poverty))
-    .attr("y", d => yLinearScale(d.obesity))
-    .style("font-size", "13px")
+    .attr("x", d => xLinearScale(d.age))
+    .attr("y", d => yLinearScale(d.smokes))
+    .style("font-size", "15px")
     .style("text-anchor", "middle")
-    .style('fill', 'white')
+    .style('fill', "white")
     .text(d => (d.abbr));
 
   // Step 6: Initialize tool tip
@@ -112,7 +104,7 @@ function successHandle(statesData) {
     .attr("class", "tooltip")
     .offset([80, -60])
     .html(function (d) {
-      return (`${d.state}<br>Poverty: ${d.poverty}%<br>Obesity: ${d.obesity}% `);
+      return (`${d.state}<br>Age: ${d.age}%<br>Smokers: ${d.smokes}% `);
     });
 
   // Step 7: Create tooltip in the chart
@@ -121,11 +113,11 @@ function successHandle(statesData) {
 
   // Step 8: Create event listeners to display and hide the tooltip
   // ==============================
-  circlesGroup.on("mouseover", function (data) {
+  circlesGroup.on("click", function (data) {
     toolTip.show(data, this);
   })
     // onmouseout event
-    .on("mouseout", function (data) {
+    .on("mouseout", function (data, index) {
       toolTip.hide(data);
     });
 
@@ -136,10 +128,10 @@ function successHandle(statesData) {
     .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
     .attr("class", "axisText")
-    .text("Obese (%)");
+    .text("Smokes (%)");
 
   chartGroup.append("text")
     .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
     .attr("class", "axisText")
-    .text("In Poverty (%)");
+    .text("Age (%)");
 }
